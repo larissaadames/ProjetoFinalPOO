d9906c4a14ec35d380f601fe961329cdd6f80218
Larissa Adames <larissaadames0@gmail.com>
2025-11-07 14:19:59 -0300

    test de branch

--- src/main/java/com/example/projetopoo/Launcher.java
+++ src/main/java/com/example/projetopoo/Launcher.java
@@ -4,7 +4,7 @@ import javafx.application.Application;
 
 public class Launcher {
     public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
+        Application.launch(Main.class, args);
     }
 }
 //teste
