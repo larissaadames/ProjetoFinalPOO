490dac32eca90197b4b161119860cd0b5f1a2596
Davi Cagnato <betocagnato@gmail.com>
2025-11-12 14:30:33 -0300

    add imagens pro jogo

--- src/main/java/com/example/projetopoo/Main.java
+++ src/main/java/com/example/projetopoo/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -17,34 +18,41 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        Group rootMenu = new Group();
-        Scene cenaMenu = new Scene(rootMenu, 1920,1080);
-        Button botaoJogar = new Button("JOGAR");
-        botaoJogar.setPrefWidth(300);
-        botaoJogar.setPrefHeight(250);
-        botaoJogar.setLayoutX(960);
-        botaoJogar.setLayoutY(540);
+        Parent rootSeletorMusica = FXMLLoader.load
+                (getClass().getResource("cenaSeletorMusica.fxml"));
 
 
-        Text titulo = new Text("POWERJORGE");
-
-        rootMenu.getChildren().add(botaoJogar);
-        rootMenu.getChildren().add(titulo);
-        stage.setScene(cenaMenu);
-
-        Group root = new Group();
-        Scene cenaJogo = new Scene(root, 1920, 1080);
-        stage.setTitle("Gameplay");
-        stage.setFullScreen(true);
-
-        botaoJogar.setOnAction(evento -> stage.setScene(cenaJogo) );
-        // stage.setScene(cenaJogo);
+        Scene cenaSeletor = new Scene(rootSeletorMusica);
+        stage.setScene(cenaSeletor);
         stage.show();
-
-
-
-        Jogo jogo = new Jogo(root);
-        jogo.iniciar();
+//        Group rootMenu = new Group();
+//        Scene cenaMenu = new Scene(rootMenu, 1920,1080);
+//        Button botaoJogar = new Button("JOGAR");
+//        botaoJogar.setPrefWidth(300);
+//        botaoJogar.setPrefHeight(250);
+//        botaoJogar.setLayoutX(960);
+//        botaoJogar.setLayoutY(540);
+//
+//
+//        Text titulo = new Text("POWERJORGE");
+//
+//        rootMenu.getChildren().add(botaoJogar);
+//        rootMenu.getChildren().add(titulo);
+//        stage.setScene(cenaMenu);
+//
+//        Group root = new Group();
+//        Scene cenaJogo = new Scene(root, 1920, 1080);
+//        stage.setTitle("Gameplay");
+//        stage.setFullScreen(true);
+//
+//        botaoJogar.setOnAction(evento -> stage.setScene(cenaJogo) );
+//        // stage.setScene(cenaJogo);
+//        stage.show();
+//
+//
+//
+//        Jogo jogo = new Jogo(root);
+//        jogo.iniciar();
 
 
         
