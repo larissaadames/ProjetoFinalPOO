694dde817c3dae4d5658ed349eeb97d38337aa36
Larissa Adames <larissaadames0@gmail.com>
2025-11-12 15:47:44 -0300

    getters e setters

--- src/main/java/com/example/projetopoo/Jogador.java
+++ src/main/java/com/example/projetopoo/Jogador.java
@@ -9,4 +9,64 @@ public class Jogador {
     private LocalDateTime criadoEm;
     private LocalDateTime ultimaPartida;
     private int totalPartidas;
+
+    public Jogador(String nickName, float scoreMax, int ID, LocalDateTime criadoEm, LocalDateTime ultimaPartida, int totalPartidas) {
+        this.nickName = nickName;
+        this.scoreMax = scoreMax;
+        this.ID = ID;
+        this.criadoEm = criadoEm;
+        this.ultimaPartida = ultimaPartida;
+        this.totalPartidas = totalPartidas;
+    }
+
+
+
+    public int getTotalPartidas() {
+        return totalPartidas;
+    }
+
+    public void setTotalPartidas(int totalPartidas) {
+        this.totalPartidas = totalPartidas;
+    }
+
+    public LocalDateTime getUltimaPartida() {
+        return ultimaPartida;
+    }
+
+    public void setUltimaPartida(LocalDateTime ultimaPartida) {
+        this.ultimaPartida = ultimaPartida;
+    }
+
+    public LocalDateTime getCriadoEm() {
+        return criadoEm;
+    }
+
+    public void setCriadoEm(LocalDateTime criadoEm) {
+        this.criadoEm = criadoEm;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public float getScoreMax() {
+        return scoreMax;
+    }
+
+    public void setScoreMax(float scoreMax) {
+        this.scoreMax = scoreMax;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
 }
