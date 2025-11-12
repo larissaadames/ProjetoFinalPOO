7c8e78bdcdbc012b4c2a9f9c9a8b03405d8c03e6
Larissa Adames <larissaadames0@gmail.com>
2025-11-12 14:58:15 -0300

    jogador

--- src/main/java/com/example/projetopoo/Jogador.java
+++ src/main/java/com/example/projetopoo/Jogador.java
@@ -1,7 +1,12 @@
 package com.example.projetopoo;
 
+import java.time.LocalDateTime;
+
 public class Jogador {
     private String nickName;
     private float scoreMax;
-
+    private int ID;
+    private LocalDateTime criadoEm;
+    private LocalDateTime ultimaPartida;
+    private int totalPartidas;
 }
