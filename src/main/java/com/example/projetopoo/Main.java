199ed489b6fdf6beb823741cf3cab9221fe176bd
quinti <quintitecnologias@gmail.com>
2025-11-12 11:59:44 -0300

    menu muito basico que leva pro jogo muito basico

--- src/main/java/com/example/projetopoo/Main.java
+++ src/main/java/com/example/projetopoo/Main.java
@@ -2,8 +2,13 @@ package com.example.projetopoo;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,12 +17,31 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
+        Group rootMenu = new Group();
+        Scene cenaMenu = new Scene(rootMenu, 1920,1080);
+        Button botaoJogar = new Button("JOGAR");
+        botaoJogar.setPrefWidth(300);
+        botaoJogar.setPrefHeight(250);
+        botaoJogar.setLayoutX(960);
+        botaoJogar.setLayoutY(540);
+
+
+        Text titulo = new Text("POWERJORGE");
+
+        rootMenu.getChildren().add(botaoJogar);
+        rootMenu.getChildren().add(titulo);
+        stage.setScene(cenaMenu);
+
         Group root = new Group();
-        Scene cena = new Scene(root, 960, 540);
+        Scene cenaJogo = new Scene(root, 1920, 1080);
         stage.setTitle("Gameplay");
         stage.setFullScreen(true);
-        stage.setScene(cena);
+
+        botaoJogar.setOnAction(evento -> stage.setScene(cenaJogo) );
+        // stage.setScene(cenaJogo);
         stage.show();
+
+
 
         Jogo jogo = new Jogo(root);
         jogo.iniciar();
